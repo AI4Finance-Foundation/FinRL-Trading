70e7bb7deb4d09a335a3ac8af2916f3519c05542
Bruce Yang <hongyang.yang@columbia.edu>
2020-09-27 16:02:56 -0400

    Update preprocessors.py

--- preprocessing/preprocessors.py
+++ preprocessing/preprocessors.py
@@ -45,48 +45,48 @@ def calcualte_price(df):
     return data
 
 def add_technical_indicator(df):
-        """
-        calcualte technical indicators
-        use stockstats package to add technical inidactors
-        :param data: (df) pandas dataframe
-        :return: (df) pandas dataframe
-        """
-        stock = Sdf.retype(df.copy())
+    """
+    calcualte technical indicators
+    use stockstats package to add technical inidactors
+    :param data: (df) pandas dataframe
+    :return: (df) pandas dataframe
+    """
+    stock = Sdf.retype(df.copy())
 
-        stock['close'] = stock['adjcp']
-        unique_ticker = stock.tic.unique()
+    stock['close'] = stock['adjcp']
+    unique_ticker = stock.tic.unique()
 
-        macd = pd.DataFrame()
-        rsi = pd.DataFrame()
-        cci = pd.DataFrame()
-        dx = pd.DataFrame()
+    macd = pd.DataFrame()
+    rsi = pd.DataFrame()
+    cci = pd.DataFrame()
+    dx = pd.DataFrame()
 
-        #temp = stock[stock.tic == unique_ticker[0]]['macd']
-        for i in range(len(unique_ticker)):
-            ## macd
-            temp_macd = stock[stock.tic == unique_ticker[i]]['macd']
-            temp_macd = pd.DataFrame(temp_macd)
-            macd = macd.append(temp_macd, ignore_index=True)
-            ## rsi
-            temp_rsi = stock[stock.tic == unique_ticker[i]]['rsi_30']
-            temp_rsi = pd.DataFrame(temp_rsi)
-            rsi = rsi.append(temp_rsi, ignore_index=True)
-            ## cci
-            temp_cci = stock[stock.tic == unique_ticker[i]]['cci_30']
-            temp_cci = pd.DataFrame(temp_cci)
-            cci = cci.append(temp_cci, ignore_index=True)
-            ## adx
-            temp_dx = stock[stock.tic == unique_ticker[i]]['dx_30']
-            temp_dx = pd.DataFrame(temp_dx)
-            dx = dx.append(temp_dx, ignore_index=True)
+    #temp = stock[stock.tic == unique_ticker[0]]['macd']
+    for i in range(len(unique_ticker)):
+        ## macd
+        temp_macd = stock[stock.tic == unique_ticker[i]]['macd']
+        temp_macd = pd.DataFrame(temp_macd)
+        macd = macd.append(temp_macd, ignore_index=True)
+        ## rsi
+        temp_rsi = stock[stock.tic == unique_ticker[i]]['rsi_30']
+        temp_rsi = pd.DataFrame(temp_rsi)
+        rsi = rsi.append(temp_rsi, ignore_index=True)
+        ## cci
+        temp_cci = stock[stock.tic == unique_ticker[i]]['cci_30']
+        temp_cci = pd.DataFrame(temp_cci)
+        cci = cci.append(temp_cci, ignore_index=True)
+        ## adx
+        temp_dx = stock[stock.tic == unique_ticker[i]]['dx_30']
+        temp_dx = pd.DataFrame(temp_dx)
+        dx = dx.append(temp_dx, ignore_index=True)
 
 
-        df['macd'] = macd
-        df['rsi'] = rsi
-        df['cci'] = cci
-        df['adx'] = dx
+    df['macd'] = macd
+    df['rsi'] = rsi
+    df['cci'] = cci
+    df['adx'] = dx
 
-        return df
+    return df
 
 
 
